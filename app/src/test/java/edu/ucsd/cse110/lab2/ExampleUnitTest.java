4464ca69cd8069232a2ed764da2e898911f1ca6d
AchuthKrishna <92132574+AchuthKrishna@users.noreply.github.com>
2023-01-16 14:16:50 -0800

    Adding betterTest to Example file to see if it runs

--- app/src/test/java/edu/ucsd/cse110/lab2/ExampleUnitTest.java
+++ app/src/test/java/edu/ucsd/cse110/lab2/ExampleUnitTest.java
@@ -4,6 +4,10 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.lifecycle.Lifecycle;
 import androidx.test.core.app.ActivityScenario;
 
 /**
@@ -15,5 +19,27 @@ public class ExampleUnitTest {
     @Test
     public void should_always_pass() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void test_one_plus_one_equals_two() {
+        var scenario = ActivityScenario.launch(MainActivity.class);
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.STARTED);
+
+        scenario.onActivity(activity -> {
+            Button btn1 = activity.findViewById(R.id.btn_one);
+            Button plus = activity.findViewById(R.id.btn_plus);
+            Button equal = activity.findViewById(R.id.btn_equals);
+
+            btn1.performClick();
+            plus.performClick();
+            btn1.performClick();
+            equal.performClick();
+
+            TextView disp = activity.findViewById(R.id.display);
+
+            assertEquals(2, disp.getText());
+        });
     }
 }
