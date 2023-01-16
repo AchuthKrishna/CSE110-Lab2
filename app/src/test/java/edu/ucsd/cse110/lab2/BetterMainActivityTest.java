c108fee27bc64cbddfc7331788713999fc6b9442
AchuthKrishna <92132574+AchuthKrishna@users.noreply.github.com>
2023-01-16 14:30:53 -0800

    Fix string comparison issue in test

--- app/src/test/java/edu/ucsd/cse110/lab2/BetterMainActivityTest.java
+++ app/src/test/java/edu/ucsd/cse110/lab2/BetterMainActivityTest.java
@@ -1,2 +1,38 @@
-package edu.ucsd.cse110.lab2;public class BetterMainActivityTest {
+package edu.ucsd.cse110.lab2;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+
+@RunWith(RobolectricTestRunner.class)
+public class BetterMainActivityTest {
+    @Test
+    public void test_one_plus_one_equals_two() {
+        var scenario = ActivityScenario.launch(MainActivity.class);
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.STARTED);
+
+        scenario.onActivity(activity -> {
+            Button btn1 = activity.findViewById(R.id.btn_one);
+            Button plus = activity.findViewById(R.id.btn_plus);
+            Button equal = activity.findViewById(R.id.btn_equals);
+
+            btn1.performClick();
+            plus.performClick();
+            btn1.performClick();
+            equal.performClick();
+
+            TextView disp = activity.findViewById(R.id.display);
+
+            assertEquals("2", disp.getText());
+        });
+    }
 }
